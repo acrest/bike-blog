2c86a3ae180fba40c27b43e71fcb2df177275d8f
acrest <aleccrestani@gmail.com>
2022-12-15 21:51:34 -0700

    firebase init

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,26 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+// Firebase
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 if (environment.production) {
   enableProdMode();
 }
+
+// Firebase Config
+const firebaseConfig = {
+	apiKey: "AIzaSyCHIITNQ3YhM3MrtCXXuVLWw_ywwPyE0HE",
+	authDomain: "alecs-bike-blog.firebaseapp.com",
+	projectId: "alecs-bike-blog",
+	storageBucket: "alecs-bike-blog.appspot.com",
+	messagingSenderId: "585148816145",
+	appId: "1:585148816145:web:e5e8a6f7257c11f1830ab0",
+	measurementId: "G-F2W8DH71NC"
+  };
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
