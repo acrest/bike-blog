7cdffd0f49457b9b5dd96a03489b4c4a492603fa
acrest <aleccrestani@gmail.com>
2022-12-22 00:05:55 -0700

    add production firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+	  apiKey: "AIzaSyCHIITNQ3YhM3MrtCXXuVLWw_ywwPyE0HE",
+	  authDomain: "alecs-bike-blog.firebaseapp.com",
+	  projectId: "alecs-bike-blog",
+	  storageBucket: "alecs-bike-blog.appspot.com",
+	  messagingSenderId: "585148816145",
+	  appId: "1:585148816145:web:e5e8a6f7257c11f1830ab0",
+	  measurementId: "G-F2W8DH71NC"
+	}
 };
